525783c175905b7d1322a11c0710345547564648
jeanMachadoNotes <machadonote@gmail.com>
2026-02-12 10:57:11 -0600

    update contact form

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ class ContactMessage(BaseModel):
     name: str = Field(min_length=2, max_length=100)
     email: EmailStr
     message: str = Field(min_length=10, max_length=2000)
+    company: str | None = None
 
 app = FastAPI()
 
